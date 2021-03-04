f97a87ff98d15913e6f15ad3d61f1db6cc4e982b
lelaum <73001613+lelaum@users.noreply.github.com>
2021-03-03 22:10:12 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 
 const firebaseConfig = {	
     apiKey: "${VAR_API}",
-    authDomain: "${VAR_AUT",
+    authDomain: "${VAR_AUT}",
     projectId: "${VAR_PROJECT}",
     storageBucket: "${VAR_STORAGE}",
     messagingSenderId: "${VAR_MESS}",
